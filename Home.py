1d759f8a2227dba2c1c47e782d0328be0cae0163
Samiksha Shukla <152069982+samiksha0shukla@users.noreply.github.com>
2025-07-05 18:00:27 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to HomeSage! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.image("public/ChatGPT Image Jul 3, 2025, 07_29_03 PM.png")
